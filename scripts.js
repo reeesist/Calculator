abfc3b70fd1f5969f1512780b3f5b45f87c1661e
Lukas <lpovilaitis@yahoo.com>
2022-08-11 17:00:32 +0100

    added functions for calculatin

---
+++ scripts.js
@@ -0,0 +1,29 @@
+function add(a, b) {
+    return sum = a + b
+}
+
+function subtract(a, b) {
+    return sum = a - b
+}
+
+function multiply(a, b) {
+    return sum = a * b
+}
+
+function divide(a, b) {
+    return sum = a / b
+}
+
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "*":
+            return multiply(a, b);
+        case "/":
+            return divide(a, b);
+    }
+}
